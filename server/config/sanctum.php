c33950c88607401a1a0c31d80b1bd91c2225dace
smctdev <168695959+smctdev@users.noreply.github.com>
2025-11-28 11:05:24 +0800

    updated sanctum

--- server/config/sanctum.php
+++ server/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,192.168.1.1,127.0.0.1:8000,localhost:4000,192.168.1.1:7030,::1',
+        'localhost,localhost:3000,127.0.0.1,192.168.1.1,127.0.0.1:8000,localhost:4000,192.168.1.1:7030,::1,https://beta-ticketing.smctgroup.ph',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),

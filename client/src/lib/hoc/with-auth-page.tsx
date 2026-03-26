5065994b81bf38636c9cc90fe9c2b4b276d509e5
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-03-26 08:35:25 +0800

    added redirecting swal;

--- client/src/lib/hoc/with-auth-page.tsx
+++ client/src/lib/hoc/with-auth-page.tsx
@@ -3,10 +3,11 @@ import PreLoader from "@/components/loaders/pre-loader";
 import { ROLE } from "@/constants/roles";
 import { useAuth } from "@/context/auth-context";
 import { redirect } from "next/navigation";
+import Swal from "sweetalert2";
 
 export default function withAuthPage(
   WrappedComponent: any,
-  isProtected = false
+  isProtected = false,
 ) {
   function AppWrappedComponent(props: any) {
     const { isAuthenticated, user, isLoading } = useAuth();
@@ -14,7 +15,7 @@ export default function withAuthPage(
     const noAccess =
       isProtected &&
       ![ROLE.ADMIN, ROLE.AUTOMATION_ADMIN].includes(
-        user?.user_role?.role_name
+        user?.user_role?.role_name,
       ) &&
       isAlreadyAuthenticated;
 
@@ -27,6 +28,15 @@ export default function withAuthPage(
     }
 
     if (!isAlreadyAuthenticated) {
+      Swal.fire({
+        title: "Redirecting...",
+        text: "Redirecting to login. Please wait...",
+        allowOutsideClick: false,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      });
+
       return redirect("/login");
     }
 
